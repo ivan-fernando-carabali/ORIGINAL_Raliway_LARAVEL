da9387a41d1adaa6b1575504be83a49a5776a6cc
Iván Fernando Carabali <vansfernando77@hotmail.com>
2025-11-25 23:41:31 -0500

    configuracion del cros para railwy laravel

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,29 @@
 <?php
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+
+    'allowed_origins' => [
+        'https://localhost',
+        'https://localhost:8100',
+        'capacitor://localhost',
+        'ionic://localhost',
+        'http://localhost',
+        'http://localhost:8100',
+        'http://localhost:4200',
+        'https://smartinventori-production.up.railway.app',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => false,
+
+    'supports_credentials' => true,
 ];
+
